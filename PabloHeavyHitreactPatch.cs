85beab892e2e8e2dfaec27218172a3395d940257
GreatWyrm <alecmusante@gmail.com>
2024-09-02 13:48:51 -0700

    Change to always stagger pablo due to technical limitations

--- PabloHeavyHitreactPatch.cs
+++ PabloHeavyHitreactPatch.cs
@@ -21,14 +21,9 @@ public class PabloHeavyHitreactPatch {
     [HarmonyPrefix]
     static bool HavePabloStagger(Dam_EnemyDamageBase __instance, ref ES_HitreactType hitreact, ref bool tryForceHitreact, float damage, float staggerDamageMulti) {
         if (__instance.Owner.EnemyDataID == 47) {
-            float totalStagger = __instance.m_damBuildToHitreact + damage * staggerDamageMulti;
-            logger.LogInfo("Total Stagger: " + totalStagger);
-            logger.LogInfo($"Damage: {damage}, Stagger Multi: {staggerDamageMulti}.");
-            if (totalStagger >= __instance.Owner.EnemyBalancingData.Health.DamageUntilHitreact) {
-                logger.LogInfo("Forcing a pablo stagger");
-                hitreact = ES_HitreactType.Heavy;
-                tryForceHitreact = true;
-            }
+            logger.LogDebug("Forcing a pablo stagger");
+            hitreact = ES_HitreactType.Heavy;
+            tryForceHitreact = true;
         }
         return true;
     }
